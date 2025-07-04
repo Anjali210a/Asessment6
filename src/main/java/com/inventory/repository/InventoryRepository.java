dc668844c009d3ef21fa13daf85739bd5db0f50f
Anjali gupta <anjaligupta3414@gmail.com>
2025-07-04 23:12:38 +0530

    Delete src directory

--- src/main/java/com/inventory/repository/InventoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.inventory.repository;
-
-import com.inventory.model.InventoryItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<InventoryItem, Long> {}
